3497755c2ceae5682b6868b81049b4bd054fcb5a
Christian Chu <66145369+christianchu@users.noreply.github.com>
2020-10-12 19:16:10 -0700

    clogic update

--- static/js/clogic.js
+++ static/js/clogic.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Create the tile layer that will be the background of the map
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -8,25 +7,35 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
 });
 
   // Create a baseMaps object to hold the lightmap layer
-var layers = {
-    AVAILABLE: new L.LayerGroup(),
-    TOP_10: new L.LayerGroup(),
-    EMPTY: new L.LayerGroup()
-};
+var Available = new L.LayerGroup(),
+    Top_10 = new L.LayerGroup(),
+    Empty = new L.LayerGroup()
 
   // Create the map object with layers
 var map = L.map("map-id", {
-    center: [37.0902, -95.7129],
+    center: [42.3601, -71.0589],
     zoom: 12,
-    layers: [
-        layers.AVAILABLE,
-        layers.TOP_10,
-        layers.EMPTY
-    ]
+    layers: [lightmap]
 });
+
+// baseMap object
+var baseMap = {
+    "Light Map": lightmap
+  };
 
 // Add our 'lightmap' tile layer to the map
 lightmap.addTo(map);
+
+// Create an overlayMaps object to hold the layers of data
+var overlayMaps = {
+    "Availables": availables,
+    "top10s": top10s
+  };
+
+// Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+L.control.layers(baseMap, overlayMaps, { 
+    collapsed: false
+  }).addTo(map);
 
 // Create an overlayMaps object to hold the available, top10, empty layers
 var overlayMaps = {
@@ -36,43 +45,4 @@ var overlayMaps = {
 };
 
 // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-=======
-// Create the tile layer that will be the background of the map
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "light-v10",
-    accessToken: API_KEY
-});
-
-  // Create a baseMaps object to hold the lightmap layer
-var layers = {
-    AVAILABLE: new L.LayerGroup(),
-    TOP_10: new L.LayerGroup(),
-    EMPTY: new L.LayerGroup()
-};
-
-  // Create the map object with layers
-var map = L.map("map-id", {
-    center: [37.0902, -95.7129],
-    zoom: 12,
-    layers: [
-        layers.AVAILABLE,
-        layers.TOP_10,
-        layers.EMPTY
-    ]
-});
-
-// Add our 'lightmap' tile layer to the map
-lightmap.addTo(map);
-
-// Create an overlayMaps object to hold the available, top10, empty layers
-var overlayMaps = {
-    "Available": layers.AVAILABLE,
-    "Top 10": layers.TOP_10,
-    "EMPTY": layers.EMPTY
-};
-
-// Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
->>>>>>> 70c8bea36cea16fe0a7ef80fff3a6beb57c7dd79
 L.control.layers(null, overlays).addTo(map);
