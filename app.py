9af04b18f438db1ec00b74b0e17585f426033830
Noe Torres <ntorr012@ucr.edu>
2020-10-17 16:42:50 -0700

    managed to access data but only directly through html code

--- app.py
+++ app.py
@@ -23,38 +23,6 @@ Base.prepare(engine, reflect=True)
 Review = Base.classes.reviews
 Listing = Base.classes.listings
 
-#function to collect the listings info
-def getAllListings():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    # results is a list of tuples
-    results = session.query(Listing.index, Listing.id, Listing.listing_url, Listing.name, Listing.price_y, Listing.number_of_reviews, Listing.review_scores_rating, Listing.date, Listing.available, Listing.neighbourhood, Listing.latitude, Listing.longitude, Listing.property_type, Listing.bedrooms, Listing.beds, Listing.zipcode).all()
-    session.close()
-    listings = []
-    for row in results:
-        listing = {}
-        listing['index'] = row.index
-        listing['id'] = row.id
-        listing['listing_url'] = row.listing_url
-        listing['name'] = row.name
-        listing['price_y'] = row.price
-        listing['number_of_reviews'] = row.number_of_reviews
-        listing['review_scores_rating'] = row.review_scores_rating
-        listing['date'] = row.date
-        listing['available'] = row.available
-        listing['neighbourhood'] = row.neighbourhood
-        listing['latitude'] = row.latitude
-        listing['longitude'] = row.longitude
-        listing['property_type'] = row.property_type
-        listing['bedrooms'] = row.bedrooms
-        listing['beds'] = row.beds
-        #added the zipcode column to your code in case you wanted or needed it
-        listing['zipcode'] = row.zipcode
-        listings.append(listing)
-
-    return jsonify(listings)
-
 # Flask Setup
 app = Flask(__name__)
 
@@ -78,7 +46,7 @@ def getAllListings():
         listing['id'] = row.id
         listing['listing_url'] = row.listing_url
         listing['name'] = row.name
-        listing['price_y'] = row.price
+        listing['price_y'] = row.price_y
         listing['number_of_reviews'] = row.number_of_reviews
         listing['review_scores_rating'] = row.review_scores_rating
         listing['date'] = row.date
@@ -96,7 +64,7 @@ def getAllListings():
     return jsonify(listings)
 
 @app.route("/comparisons")
-def comparions():
+def comparisons():
     return render_template('comparisons.html')
 
 @app.route("/map")
