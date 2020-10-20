0333c6ce5b2c1f2eba8bd0cf5b52fe1860e8953a
Christian Chu <66145369+christianchu@users.noreply.github.com>
2020-10-19 17:25:15 -0700

    trying top 25

--- templates/static/js/clogic.js
+++ templates/static/js/clogic.js
@@ -23,3 +23,29 @@ d3.csv("Resources/listings.csv", function(data) {
       .addTo(myMap)
       .bindPopup("Listing Name: " + listing.name + "<br>Number of Reviews: " + listing.number_of_reviews)
 }});
+
+var greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var highReviews = [];
+
+d3.csv("Resources/listings.csv", function(data) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].number_of_reviews > 200) {
+      highReviews.push(data[i]);
+      }
+  }console.log(highReviews);
+})
+
+L.marker([
+  parseFloat(highReviews["latitude"]),
+  parseFloat(highReviews["longitude"])],
+  {icon: greenIcon})
+  .addTo(myMap)
+  .bindPopup("Listing Name: " + highReviews.name + "<br>Number of Reviews: " + highReviews.number_of_reviews)
